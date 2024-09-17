8b05e88bc9a010cc0d6c16ef99db8c1f82bd6583
cesar <cesarxx_19@hotmail.com>
2024-09-17 08:40:05 -0500

    Se agrego el repositorio sis biblioteca

---
+++ sprint-boot-app/src/main/java/proyecto/sprint_boot_app/SprintBootAppApplication.java
@@ -0,0 +1,13 @@
+package proyecto.sprint_boot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootAppApplication.class, args);
+	}
+
+}
---
+++ sprint-boot-app/src/test/java/proyecto/sprint_boot_app/SprintBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.sprint_boot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
