d6ab11565589b5e01561af29d49bae89db0a5bec
cristhian <kcaladj123@gmail.com>
2024-09-17 09:10:04 -0500

    clase Libro.java

---
+++ spring-boot-app/src/main/java/entity/Libro.java
@@ -0,0 +1,62 @@
+package entity;
+
+import jakarta.persistence.Id;
+
+public class Libro {
+    private String id;
+    private String autor;
+    private Integer anio;
+    private String categoria;
+    private Integer cantidad;
+
+    public Libro() {
+    }
+
+    public Libro(String id, String autor, Integer anio, String categoria, Integer cantidad) {
+        this.id = id;
+        this.autor = autor;
+        this.anio = anio;
+        this.categoria = categoria;
+        this.cantidad = cantidad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAnio() {
+        return anio;
+    }
+
+    public void setAnio(Integer anio) {
+        this.anio = anio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+}
