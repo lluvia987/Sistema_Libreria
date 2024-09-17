9b440150eb3419f4cd54edb4447ec00d614990e3
cristhian <kcaladj123@gmail.com>
2024-09-17 12:51:18 -0500

    creacion de LibroRepository

--- spring-boot-app/src/main/java/example/sis_libreria2/repository/LibroRepository.java
+++ spring-boot-app/src/main/java/example/sis_libreria2/repository/LibroRepository.java
@@ -1,5 +1,10 @@
 package example.sis_libreria2.repository;
 
-public interface LibroRepository {
+import example.sis_libreria2.entity.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LibroRepository  extends JpaRepository <Libro, Integer> {
 }
 
